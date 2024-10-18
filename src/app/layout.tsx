767e6a408e757dc0e3732b6c9fbfd2e950978eb4
jwo0o0 <jwo0o0358@gmail.com>
2024-10-19 00:39:06 +0900

    feat: Pretendard 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { StyledWrapper } from "@/style/styled-wrapper";
+import "@/style/global.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
