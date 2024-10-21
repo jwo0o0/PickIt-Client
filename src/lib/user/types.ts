58ed49d26872f572039bfdcd21317decb4c74a88
김정우 <jwo0o0358@gmail.com>
2024-10-21 17:57:40 +0900

    Revert "[Feat] 개발 환경 설정, 앱 라우터 설정 (#2)" (#4)
    This reverts commit 317f001a6c6cdd6c33774f57ae998233deba6d25.

--- src/lib/user/types.ts
+++
@@ -1,4 +0,0 @@
-export interface UploadProfileImageResponse {
-  message: string;
-  profileImage: string;
-}
--- src/store/auth/types.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "@/lib/auth/types";
-export interface AuthStore {
-  isLogin: boolean;
-  user: User | null;
-  setIsLogin: (isLogin: boolean) => void;
-  setUser: (user: User) => void;
-  logout: () => void;
-}
