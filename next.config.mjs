778bd329dd2b01c7f6c201c62f23a1f45a58f7e5
jwo0o0 <jwo0o0358@gmail.com>
2024-10-23 18:44:50 +0900

    feat: rewrites 설정

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  async rewrites() {
+    return [
+      {
+        destination: `${process.env.NEXT_PUBLIC_PROD_API_URL}`,
+        source: "/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
