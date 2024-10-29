771407e1a5bc602454ec4d74889ec4a4ac8d85e2
jwo0o0 <jwo0o0358@gmail.com>
2024-10-29 22:39:32 +0900

    feat: 피드 상세 페이지 구현

--- src/app/(routes)/feed/[id]/page.tsx
+++ src/app/(routes)/feed/[id]/page.tsx
@@ -1,5 +1,27 @@
 import NavbarWrapper from "@/components/layout/Navbar/NavbarWrapper";
+import { ContentHeader } from "@/components/common/ContentHeader";
+import { FeedContent } from "@/components/feed/FeedContent";
 
-export default function FeedPage() {
-  return <NavbarWrapper></NavbarWrapper>;
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+import { prefetchFeed } from "@/lib/feed/hooks/useGetFeed";
+
+export default async function FeedPage({ params }: { params: { id: string } }) {
+  const queryClient = new QueryClient();
+  await prefetchFeed(Number(params.id), queryClient);
+
+  return (
+    <>
+      <ContentHeader title="" />
+      <div className="pt-24">
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <FeedContent feedId={Number(params.id)} />
+        </HydrationBoundary>
+      </div>
+      <NavbarWrapper />
+    </>
+  );
 }
