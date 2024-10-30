ef511d0313e830f8a3349ec4948a661dd165666d
jwo0o0 <jwo0o0358@gmail.com>
2024-10-30 20:16:53 +0900

    style: 로그인 배너, 네비게이션 바에 z-index 추가

--- src/components/layout/Navbar/BottomNavbar.tsx
+++ src/components/layout/Navbar/BottomNavbar.tsx
@@ -24,7 +24,7 @@ export const BottomNavbar = () => {
     },
   ];
   return (
-    <div className="w-full h-[68px] bg-white bg-opacity-85 flex justify-around align-middle fixed left-0 right-0 bottom-0 md:hidden">
+    <div className="z-40 w-full h-[68px] bg-white bg-opacity-85 flex justify-around align-middle fixed left-0 right-0 bottom-0 md:hidden">
       {links.map((el) => {
         return <NavbarButton key={el.name} href={el.href} name={el.name} />;
       })}
