dc94756e260268eb317b38f1073d1f8c06488f3c
jwo0o0 <jwo0o0358@gmail.com>
2024-11-01 03:07:49 +0900

    fix: layout.tsx에서 안쓰는 코드 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,19 +10,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  modal,
 }: Readonly<{
   children: React.ReactNode;
-  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>
         <Providers>
-          <MainLayout>
-            {children}
-            {modal}
-          </MainLayout>
+          <MainLayout>{children}</MainLayout>
         </Providers>
       </body>
     </html>
