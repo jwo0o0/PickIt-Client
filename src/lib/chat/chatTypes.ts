535498234051d6f8e7bb68c7d5671d9424bf769f
jwo0o0 <jwo0o0358@gmail.com>
2024-11-06 17:31:56 +0900

    feat: 채팅 관련 커스텀 hook 정의

--- src/lib/chat/chatTypes.ts
+++ src/lib/chat/chatTypes.ts
@@ -7,5 +7,24 @@ export interface ChatRoomListType {
   roomId: number;
   latestMessage: string;
   updatedAt: string;
-  otherUser: OtherUserType | null;
+  otherUser: OtherUserType;
+}
+
+export interface ChatMessageType {
+  id?: number;
+  chatRoomId: number;
+  userId: number;
+  content: string;
+  createdAt: string;
+  read: boolean;
+}
+
+export interface GetMessagesResponseType {
+  messages: ChatMessageType[];
+  partner: OtherUserType;
+}
+
+export interface UseChatSocketReturn {
+  messages: ChatMessageType[];
+  sendMessage: (message: string) => void;
 }
