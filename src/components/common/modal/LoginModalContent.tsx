b842b1f52ecbdfcba6630e217c29b179042a9a96
jwo0o0 <jwo0o0358@gmail.com>
2024-11-08 16:36:38 +0900

    feat: 모달 관련 컴포넌트 정의

---
+++ src/components/common/modal/LoginModalContent.tsx
@@ -0,0 +1,5 @@
+export const LoginModalContent = () => {
+  return (
+    <div className="w-20 h-20 bg-white rounded-md">로그인이 필요합니다.</div>
+  );
+};
---
+++ src/components/common/modal/Modal.tsx
@@ -0,0 +1,44 @@
+import { useRef, useEffect, MouseEvent } from "react";
+import { createPortal } from "react-dom";
+import { useModalStore } from "@/store/modal/useModalStore";
+
+interface ModalProps {
+  children?: React.ReactNode;
+}
+export const Modal = ({ children }: ModalProps) => {
+  const isOpen = useModalStore((state) => state.isOpen);
+  const close = useModalStore((state) => state.close);
+
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [isOpen]);
+
+  const handleClickOutside = (e: MouseEvent<HTMLDivElement>) => {
+    if (modalRef.current === e.target) {
+      close();
+    }
+  };
+
+  if (!isOpen || typeof window === "undefined") return null;
+  return createPortal(
+    <div
+      ref={modalRef}
+      onClick={handleClickOutside}
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+    >
+      <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
+        {children}
+      </div>
+    </div>,
+    document.body
+  );
+};
