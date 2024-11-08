fa2a98de2cf653f372983efa1bfd04ba564f1c6a
jwo0o0 <jwo0o0358@gmail.com>
2024-11-08 17:01:59 +0900

    feat: 로그인 필요 모달 컴포넌트

--- src/components/common/modal/Modal.tsx
+++ src/components/common/modal/Modal.tsx
@@ -35,9 +35,7 @@ export const Modal = ({ children }: ModalProps) => {
       onClick={handleClickOutside}
       className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
     >
-      <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
-        {children}
-      </div>
+      {children}
     </div>,
     document.body
   );
