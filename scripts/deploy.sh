2aba3aaf8dc6dca060dd9dadfd59c4da23cb8bda
jwo0o0 <jwo0o0358@gmail.com>
2024-11-20 17:39:36 +0900

    fix: deploy script fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,9 @@ APP_DIR="/home/ubuntu/deploy"
 echo "Navigating to application directory..."
 cd $APP_DIR
 
+echo "Stopping existing PM2 processes..."
+pm2 delete sns_client || true
+
 echo "Starting application..."
 pm2 start ecosystem.config.js
 
