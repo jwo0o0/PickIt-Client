49f6136436c952b828fd6ec981affa032e2ec497
jwo0o0 <jwo0o0358@gmail.com>
2024-11-27 23:35:43 +0900

    fix: next.config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    const destinationUrl =
+      process.env.NODE_ENV === "production"
+        ? process.env.NEXT_PUBLIC_PROD_API_URL
+        : process.env.NEXT_PUBLIC_DEV_API_URL;
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${destinationUrl}/:path*`,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
