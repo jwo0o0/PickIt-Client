28c566da633c8a9cffaf23d3e1a996425cee4a32
김정우 <jwo0o0358@gmail.com>
2024-11-27 23:26:12 +0900

    [Fix] next.config 필요없는 설정 삭제 (#83)

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  compiler: {
-    styledComponents: true,
-  },
-  async rewrites() {
-    const destinationUrl =
-      process.env.NODE_ENV === "production"
-        ? process.env.NEXT_PUBLIC_PROD_API_URL
-        : process.env.NEXT_PUBLIC_DEV_API_URL;
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${destinationUrl}/:path*`,
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
