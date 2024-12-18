0e153d89cc21df2d34a3864d0f0359c6f7665db8
jwo0o0 <jwo0o0358@gmail.com>
2024-12-18 23:53:58 +0900

    fix: 피드 수정,삭제 드롭다운 중복 선언 제거

--- src/components/user/UserFeeds.tsx
+++ src/components/user/UserFeeds.tsx
@@ -1,40 +1,16 @@
 "use client";
-import { useState } from "react";
 import { Feed } from "../feed/Feed";
-import Dropdown from "@/components/common/Dropdown";
-import { FeedEditDrawer } from "@/components/feed/FeedEditDrawer";
-
-import { useAuthStore } from "@/store/auth/useAuthStore";
-import { useStore } from "@/store/useStore";
-
 import { useQueryClient } from "@tanstack/react-query";
 import { useGetUserFeeds } from "@/lib/user/hooks/useGetUserFeeds";
-import { useDeleteFeed } from "@/lib/feed/hooks/useDeleteFeed";
 import userKeys from "@/lib/user/userQueries";
 
 interface UserFeedsProps {
   userIdParam: string;
 }
 export const UserFeeds = ({ userIdParam }: UserFeedsProps) => {
-  const [isOpenDrawer, setIsOpenDrawer] = useState<boolean>(false);
-  const user = useStore(useAuthStore, (state) => state.user);
   const { data } = useGetUserFeeds(Number(userIdParam));
 
-  const { mutate: deleteFeedMutation } = useDeleteFeed();
-
   const queryClient = useQueryClient();
-  const handleClickDelete = (feedId: number) => {
-    deleteFeedMutation(
-      { feedId },
-      {
-        onSuccess: async () => {
-          await queryClient.invalidateQueries({
-            queryKey: userKeys.feeds(Number(userIdParam)),
-          });
-        },
-      }
-    );
-  };
 
   const handleVoteLikeSuccess = async () => {
     await queryClient.invalidateQueries({
@@ -49,23 +25,6 @@ export const UserFeeds = ({ userIdParam }: UserFeedsProps) => {
           key={feed.feedId}
           className="my-4 pb-6 border-b border-b-slate-300 relative"
         >
-          {String(user?.id) === userIdParam && (
-            <>
-              <Dropdown
-                handleClickDelete={() => {
-                  handleClickDelete(feed.feedId);
-                }}
-                hadleClickEdit={() => {
-                  setIsOpenDrawer(true);
-                }}
-              />
-              <FeedEditDrawer
-                isOpen={isOpenDrawer}
-                setIsOpen={setIsOpenDrawer}
-                data={feed}
-              />
-            </>
-          )}
           <Feed
             feedId={feed.feedId}
             data={feed}
